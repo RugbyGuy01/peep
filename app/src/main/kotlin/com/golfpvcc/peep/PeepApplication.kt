8b39cb9f36136010361965ddb533474173f15719
RugbyGuy01 <goodmail@golfpvcc.com>
2025-10-29 09:53:13 -0400

    Done with Building the User Repository

--- app/src/main/kotlin/com/golfpvcc/peep/PeepApplication.kt
+++ app/src/main/kotlin/com/golfpvcc/peep/PeepApplication.kt
@@ -1,11 +1,31 @@
 package com.golfpvcc.peep
 
+import com.golfpvcc.peep.infra.database.entities.UserEntity
+import com.golfpvcc.peep.infra.database.repositories.UserRepository
+import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Component
 
 @SpringBootApplication
 class PeepApplication
 
 fun main(args: Array<String>) {
 	runApplication<PeepApplication>(*args)
+}
+
+@Component // test if the database can store a record
+class Vinnie (
+    private val repository: UserRepository
+) {
+    @PostConstruct
+    fun init(){
+        repository.save(
+            UserEntity(
+                email = "gmail.com",
+                username = "loft",
+                hashedPassword = "1234"
+            )
+        )
+    }
 }
