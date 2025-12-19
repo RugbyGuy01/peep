3d1a3e4c294fcee5521d7aeb2737bf81116490d9
RugbyGuy01 <goodmail@golfpvcc.com>
2025-12-19 14:19:17 -0500

    Updated passwords

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
 plugins {
     id("peep.spring-boot-app")
 }
@@ -6,16 +8,28 @@ group = "com.golfpvcc"
 version = "0.0.1-SNAPSHOT"
 description = "Version 2 of peep"
 
+
+tasks {
+    named<BootJar>("bootJar") {
+        from(project(":notification").projectDir.resolve("src/main/resources")) {
+            into("")
+        }
+        from(project(":user").projectDir.resolve("src/main/resources")) {
+            into("")
+        }
+    }
+}
+
 dependencies {
     implementation(projects.user)
     implementation(projects.chat)
     implementation(projects.notification)
     implementation(projects.common)
+
     implementation(libs.kotlin.reflect)
     implementation(libs.spring.boot.starter.security)
 
     implementation(libs.spring.boot.starter.mail)
-
     implementation(libs.spring.boot.starter.amqp)
     implementation(libs.spring.boot.starter.data.redis)
     implementation(libs.spring.boot.starter.data.jpa)
